ab36906aa489cb926a2bca5bbbf3a688dcad4b10
finorako <finorako@student.42antananarivo.mg>
2026-08-19 16:19:43 +0300

    created parse branch, continue later

--- src/parsing/parse.py
+++ src/parsing/parse.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, Field, model_validator
 
 
 class GameModel(BaseModel):
-    players: dict = Field(...)
+    raw_data: dict[str, dict[str, str | int] | list[int]] = Field(...)
 
     @model_validator(mode='after')
     def validate_model(self) -> 'GameModel':
