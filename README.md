0cd7db2574430711d96456416ae8b52fd850393a
nyramana <nyramana@student.42antananarivo.mg>
2026-08-21 11:40:15 +0300

    refactor: Polishing code and removing unnecessary file

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## Resources
 
 - [Verify if a point is inside a circle](https://stackoverflow.com/questions/481144/equation-for-testing-if-a-point-is-inside-a-circle)
+- [Bresenham's line algorithm](https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm)
--- src/rendering/component/button.py
+++ src/rendering/component/button.py
@@ -1,8 +1,10 @@
-from ..core import ImgData
+from abc import ABC, abstractmethod
+
+from . import Sprite
 
 
-class Button:
-    def __init__(self, frames: list[ImgData]) -> None:
+class Button(ABC):
+    def __init__(self, frames: list[Sprite]) -> None:
         self.frames = frames
         self.current = frames[0]
 
@@ -10,8 +12,15 @@ class Button:
         return (
             self.current.pos_x
             < mouse_pos[0]
-            < self.current.pos_x + self.current.width
+            < self.current.pos_x + self.current.sprite.width
             and self.current.pos_x
             < mouse_pos[0]
-            < self.current.pos_x + self.current.width
+            < self.current.pos_x + self.current.sprite.width
         )
+
+    @abstractmethod
+    def clicked(self) -> str | None:
+        # Do some stuff
+        # When everything is done
+        # return the str (To change the screen/state)
+        return
